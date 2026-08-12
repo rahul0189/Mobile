5d2cbb740ec79f7568015ffd2e4b320b1ecac98a
erlove000 <love.deep3618@gmail.com>
2026-08-12 22:17:32 +0530

    Update TicketDetailScreen.jsx

--- CHAND Brothers Mobile Repair Shop/src/screens/TicketDetailScreen.jsx
+++ CHAND Brothers Mobile Repair Shop/src/screens/TicketDetailScreen.jsx
@@ -282,7 +282,6 @@ export default function TicketDetailScreen({
               )}
             </div>
           </div>
-        </div>
 
         {/* Right Side: Billing & Financial Overview */}
         <div className="detail-sidebar">
